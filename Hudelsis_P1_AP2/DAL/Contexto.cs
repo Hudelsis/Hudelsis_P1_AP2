1fd9323f430b5128d14bee87697458cbd1b7c99c
Hudelsis Antigua Mendez <antiguamendez27@gmail.com>
2021-05-29 17:21:19 +0200

    Agregar archivos de proyecto.

---
+++ Hudelsis_P1_AP2/DAL/Contexto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hudelsis_P1_AP2.DAL
+{
+    public class Contexto : DbContext
+    {
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+        }
+
+    }
+    
+}
