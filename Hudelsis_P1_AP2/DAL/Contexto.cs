f7b9e387186563acc91927f10717c69e9df8f56f
Hudelsis Antigua Mendez <antiguamendez27@gmail.com>
2021-06-03 00:52:41 +0200

    agregando el Contexto completo

--- Hudelsis_P1_AP2/DAL/Contexto.cs
+++ Hudelsis_P1_AP2/DAL/Contexto.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Hudelsis_P1_AP2.Models;
 
 namespace Hudelsis_P1_AP2.DAL
 {
     public class Contexto : DbContext
     {
+        public DbSet<Productos> Productos { get; set; }
 
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+            optionsBuilder.UseSqlite(@"Data Source = Data\Producto.db");
 
         }
 
